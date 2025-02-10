63f133d6333113c2fd3f6a48102cd5ac92923307
Gudipati Vishnu Vardhan Reddy <70902772+vishan01@users.noreply.github.com>
2025-02-10 23:35:58 +0530

    Feat: Implement player movements

--- script.js
+++ script.js
@@ -13,8 +13,16 @@ const Background={
     fillRect:(0,0,canvas.width,canvas.height)
 }
 
+
+//Possible movements for player
+const keys={
+    ArrowUp:false,
+    ArrowLeft:false,
+    ArrowRight:false
+}
+
 //Player Class + Gravity
-const gravity=0.1;
+const gravity=0.25;
 class Player{
     constructor(x=0,y=0){
         this.position={x:x,y:y};
@@ -47,8 +55,56 @@ function animate(){
     window.requestAnimationFrame(animate);
     context.fillStyle='teal';
     context.fillRect(0,0,canvas.width,canvas.height);
-    
     player.update();
+    if(keys.ArrowRight){
+        player.position.x+=5;
+    }else if(keys.ArrowLeft){
+        player.position.x-=5;
+    }
 }
 
 animate()
+
+//Key Press Event
+
+//On pressing key, player moves
+document.addEventListener('keydown',(event)=>{
+    switch(event.key){
+        case 'd':
+            keys.ArrowRight=true;
+            break;
+        case 'a':
+            keys.ArrowLeft=true;
+            break;
+        case 'w':
+            player.velocity.y=-10;
+            break;
+        case 'ArrowUp':
+            player.velocity.y=-10;
+            break;
+        case 'ArrowRight':
+            keys.ArrowRight=true;
+            break;
+        case 'ArrowLeft':
+            keys.ArrowLeft=true;
+            break;
+    }
+})
+
+//On releasing key, player stops
+document.addEventListener('keyup',(event)=>{
+    switch(event.key){
+        case 'd':
+            keys.ArrowRight=false;
+            break;
+        case 'a':
+            keys.ArrowLeft=false;
+            break;
+        case 'ArrowRight':
+            keys.ArrowRight=false;
+            break;
+        case 'ArrowLeft':
+            keys.ArrowLeft=false;
+            break;
+    }
+})
