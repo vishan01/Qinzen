658d5d468d7de623b15e0d7947a241281e7ebf5a
Gudipati Vishnu Vardhan Reddy <70902772+vishan01@users.noreply.github.com>
2025-02-10 23:16:18 +0530

    Feat: Add Gravity to object

--- script.js
+++ script.js
@@ -7,6 +7,38 @@ canvas.width=1024
 canvas.height=576
 
 
+//Background color
+const Background={
+    fillStyle:'teal',
+    fillRect:(0,0,canvas.width,canvas.height)
+}
 
-context.fillStyle='teal';
-context.fillRect(0,0,canvas.width,canvas.height);
+//Player Class
+class Player{
+    constructor(x=0,y=0){
+        this.position={x:x,y:y};
+    }
+    draw(){
+        context.fillStyle='red';
+        context.fillRect(this.position.x,this.position.y,50,50);
+    }
+    update(){
+        this.draw();
+        this.position.y=this.position.y<canvas.height-50?this.position.y+=1:canvas.height-50;
+    }
+}
+
+//player object
+const player=new Player();
+
+// Gravity Animation
+function animate(){
+
+    window.requestAnimationFrame(animate);
+    context.fillStyle='teal';
+    context.fillRect(0,0,canvas.width,canvas.height);
+    
+    player.update();
+}
+
+animate()
