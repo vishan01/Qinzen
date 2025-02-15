4db915b004b34957e69782439e568536ce62d38e
Gudipati Vishnu Vardhan Reddy <70902772+vishan01@users.noreply.github.com>
2025-02-15 22:38:41 +0530

    Feat: Add animations to player

--- player.js
+++ player.js
@@ -1,5 +1,5 @@
 class Player extends Sprite{
-    constructor(x=0,y=0,colosionBlocks,platformcollisionBlocks){
+    constructor(x=0,y=0,colosionBlocks,platformcollisionBlocks,animations){
         super(x,y,'./img/Samurai/Idle.png',6,5,0.5);
         this.position={x:x,y:y};
         this.velocity={x:0,y:1};
@@ -7,6 +7,7 @@ class Player extends Sprite{
         this.colosionBlocks=colosionBlocks;
         this.platformcollisionBlocks=platformcollisionBlocks;
         
+        this.animation=animations
     }
     
     update(){
@@ -17,13 +18,12 @@ class Player extends Sprite{
         context.fillStyle='rgba(248, 46, 46, 0.55)';
         context.fillRect(this.hitBox.x,this.hitBox.y,this.hitBox.width,this.hitBox.height);
         this.draw();
-        this.updateHitBox();
 
         this.position.x+=this.velocity.x;
-        
+        this.updateHitBox();
         this.checkHorizontalCollision();
-        
         this.applyGravity();
+        this.updateHitBox();
         this.checkVerticalCollision();
         
         
@@ -33,7 +33,7 @@ class Player extends Sprite{
         this.hitBox={
             x:this.position.x+20,
             y:this.position.y+21,
-            width:25,
+            width:26,
             height:43
         }
     }
@@ -45,56 +45,45 @@ class Player extends Sprite{
     }
 
     checkVerticalCollision(){
-        let flag=false;
         for(let i=0;i<this.colosionBlocks.length;i++){
             let block=this.colosionBlocks[i];
-            if(this.position.y+this.height>=block.position.y //top of block
+            if(this.hitBox.y+this.hitBox.height>=block.position.y //top of block
                 && 
-                this.position.y+this.height-this.hitBox.height<=block.position.y+block.height //bottom of block
+                this.hitBox.y<=block.position.y+block.height //bottom of block
                 && 
                 this.hitBox.x+this.hitBox.width>=block.position.x //left of block
                 && 
                 this.hitBox.x<=block.position.x+block.width//right of block
                 ){
-                    if(block.position.x==128 && block.position.y==368){
-                        flag=true;
-                        console.log("vertical collision")
-                        console.log(this.hitBox)
-                        console.log(this.velocity)
-                    }
+                    
                 if(this.velocity.y<0){
                     this.velocity.y=0;
-                    this.position.y=block.position.y+block.height+0.01;
+                    const offset= this.hitBox.y-this.position.y;
+                    this.position.y=block.position.y+block.height-offset+0.01;
                     break;
                 }
-                else if(this.velocity.y>0){
+                if(this.velocity.y>0){
                 this.velocity.y=0;
-                this.position.y=block.position.y-this.height-0.01;
+                const offset=this.position.y+this.height-(this.hitBox.y+this.hitBox.height);
+                this.position.y=block.position.y-this.height+offset-0.01;
                     break;
             }
             }
         }
-        if(flag)
-        console.log(this.hitBox)
     }
 
     checkHorizontalCollision(){
-        let flag=false;
         for(let i=0;i<this.colosionBlocks.length;i++){
             let block=this.colosionBlocks[i];
-            if(this.position.y+this.height>=block.position.y //top of block
+            if(this.hitBox.y+this.hitBox.height>=block.position.y //top of block
                 && 
-                this.position.y+this.height-this.hitBox.height<=block.position.y+block.height //bottom of block
+                this.hitBox.y<=block.position.y+block.height //bottom of block
                 && 
                 this.hitBox.x+this.hitBox.width>=block.position.x //left of block
                 && 
                 this.hitBox.x<=block.position.x+block.width//right of block
             ){
-                console.log(this.hitBox)
-                console.log(this)
-                console.log(block)
-                flag=true;
-                console.log(this.velocity)
+                
                 if(this.velocity.x<0){
                     this.velocity.x=0;
                     const offset= this.hitBox.x-this.position.x;
@@ -102,16 +91,14 @@ class Player extends Sprite{
                     console.log(this.position.x)
                     break;
                 }
-                else if(this.velocity.x>0){
+                if(this.velocity.x>0){
                 this.velocity.x=0;
                 const offset= this.position.x+this.width-(this.hitBox.x+this.hitBox.width);
-                this.position.x=block.position.x-offset-0.01;
+                this.position.x=block.position.x-this.width+offset-0.01;
                     break;
             }
             }
         }
-        if(flag)
-            console.log(this.hitBox)
     }
 
 
