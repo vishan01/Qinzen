6bc20f794030833dce7931d55c1a599e7029f90b
Gudipati Vishnu Vardhan Reddy <70902772+vishan01@users.noreply.github.com>
2025-02-19 02:08:49 +0530

    Feat: Implement camera panning logic and update hitbox calculations for improved collision detection

--- player.js
+++ player.js
@@ -13,8 +13,81 @@ class Player extends Sprite{
             this.animation[key].image.src=this.animation[key].imageSrc;
            
         }
+
+        this.cameraBox={
+            position:{x:this.position.x-50,y:this.position.y},
+            width:150,
+            height:100
+        }
+        this.hitBox={
+            x:this.position.x+25,
+            y:this.position.y+21,
+            width:15,
+            height:43
+        }
+
+    }
+
+
+    checkForHorizontalCollision(){
+        if(this.hitBox.x+this.hitBox.width+this.velocity.x>=576 || this.hitBox.x+this.velocity.x<=0){
+            this.velocity.x=0;
+        }
+    }
+    checkForVerticalCollision(){
+        if(this.hitBox.y+this.velocity.y<=0){
+            this.velocity.y=0;
+        }
+    }
+
+
+    shouldPanCameraToLeft({canvas,camera}){
+        const cameraBoxRight=this.cameraBox.position.x+this.cameraBox.width;
+        const scaledDownCanvasWidth=canvas.width/3;
+        const addedWidth=Math.abs(camera.position.x);;
+
+        if(cameraBoxRight>=576) return;
+
+        if(cameraBoxRight>=scaledDownCanvasWidth+addedWidth){
+            camera.position.x-=this.velocity.x;
+        }
+    }
+
+    shouldPanCameraToRight({canvas,camera}){
+        const cameraBoxLeft=this.cameraBox.position.x
+        const addedWidth=Math.abs(camera.position.x);;
+
+        if(cameraBoxLeft<=0) return;
+
+        if(cameraBoxLeft<=addedWidth){
+            camera.position.x-=this.velocity.x;
+        }
+    }
+
+    shouldPanCameraToDown({canvas,camera}){
+        const cameraBoxDown=this.cameraBox.position.y
+        const addedHeight=Math.abs(camera.position.y);;
+
+        if(cameraBoxDown+this.velocity.y<=0) return;
+
+        if(cameraBoxDown<=addedHeight){
+            camera.position.y-=this.velocity.y;
+        }
     }
     
+    shouldPanCameraToUp({canvas,camera}){
+        const cameraBoxUp=this.cameraBox.position.y+this.cameraBox.height;
+        const scaledCanvasHeight=canvas.height/3;
+        const addedHeight=Math.abs(camera.position.y);;
+
+        if(cameraBoxUp+this.velocity.y>=432) return;
+
+        if(cameraBoxUp>=scaledCanvasHeight+addedHeight){
+            camera.position.y-=this.velocity.y;
+        }
+    }
+
+
     switchSprite(key){
         if(this.image==this.animation[key].image) return;
         this.currentFrame=0;
@@ -22,19 +95,32 @@ class Player extends Sprite{
         this.frameRate=this.animation[key].frameRate;
         this.frameBuffer=this.animation[key].frameBuffer;
     }
+    updateCameraBox(){
+        this.cameraBox={
+            position:{x:this.position.x-50,y:this.position.y},
+            width:150,
+            height:100
+        }
+    }
 
 
     update(){
+       
         this.updateFrames();
         this.updateHitBox();
-        context.fillStyle='rgba(59, 153, 40, 0.5)';
-        context.fillRect(this.position.x,this.position.y,this.width,this.height);
-        context.fillStyle='rgba(248, 46, 46, 0.55)';
-        context.fillRect(this.hitBox.x,this.hitBox.y,this.hitBox.width,this.hitBox.height);
+        
+        // context.fillStyle='rgba(46, 248, 248, 0.55)';
+        // context.fillRect(this.cameraBox.position.x,this.cameraBox.position.y,this.cameraBox.width,this.cameraBox.height);
+        // context.fillStyle='rgba(59, 153, 40, 0.5)';
+        // context.fillRect(this.position.x,this.position.y,this.width,this.height);
+        // context.fillStyle='rgba(248, 46, 46, 0.55)';
+        // context.fillRect(this.hitBox.x,this.hitBox.y,this.hitBox.width,this.hitBox.height);
+        this.updateCameraBox();
         this.draw();
 
         this.position.x+=this.velocity.x;
         this.updateHitBox();
+        
         this.checkHorizontalCollision();
         this.applyGravity();
         this.updateHitBox();
@@ -45,9 +131,9 @@ class Player extends Sprite{
 
     updateHitBox(){
         this.hitBox={
-            x:this.position.x+20,
+            x:this.position.x+25,
             y:this.position.y+21,
-            width:26,
+            width:15,
             height:43
         }
     }
@@ -127,7 +213,6 @@ class Player extends Sprite{
                     this.velocity.x=0;
                     const offset= this.hitBox.x-this.position.x;
                     this.position.x=block.position.x+block.width-offset+0.01;
-                    console.log(this.position.x)
                     break;
                 }
                 if(this.velocity.x>0){
