0bd543e61dc54f135d013318d6b46d4362bf1429
Gudipati Vishnu Vardhan Reddy <70902772+vishan01@users.noreply.github.com>
2025-02-18 16:48:29 +0530

    Feat: Update collisionBlock to accept dynamic height parameter

--- collisionBlock.js
+++ collisionBlock.js
@@ -1,8 +1,8 @@
 class collisionBlock{
-    constructor(x=0,y=0){
+    constructor(x=0,y=0,height=16){
         this.position={x:x,y:y};
         this.width=16;
-        this.height=16;
+        this.height=height;
     }
     draw(){
         context.fillStyle='rgba(255, 0, 0, 0.43)';
